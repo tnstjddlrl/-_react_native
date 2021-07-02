bb01cc65c5b1e22ef7c10cc0004d1eefb841815d
DESKTOP-V0TP74J\user <tnstjddlrl@naver.com>
2021-07-02 18:03:24 +0900

    백그라운드 작업 테스트

--- src/reservation.js
+++ src/reservation.js
@@ -11,17 +11,28 @@ import {
     Dimensions
 } from 'react-native';
 
+import BackgroundTimer from 'react-native-background-timer';
+import { LocalNotification } from './noti';
+
 const Reservation = () => {
     let date1 = new Date(Date.now());
     let date2 = new Date(Date.now());
 
-    date1.setHours(16, 30, 0, 0)
-    date1.setDate(date1.getDate() + 1)
+    date1.setHours(15, 21, 30, 0)
+    // date1.setDate(date1.getDate() + 1)
 
 
     console.log(date1.valueOf())
-    console.log(date1.valueOf() - date2.valueOf())
+    console.log(date1.valueOf() < date2.valueOf())
 
+    BackgroundTimer.runBackgroundTimer(() => {
+        console.log('백그라운드 작업')
+        if (date1.valueOf() < date2.valueOf()) {
+            console.log('시간다됨')
+            LocalNotification()
+            BackgroundTimer.stopBackgroundTimer()
+        }
+    }, 9000);
 
     return (
         <View>

3758891e85c6faf1b9f733d6b67736c60d680c6b
DESKTOP-V0TP74J\user <tnstjddlrl@naver.com>
2021-07-28 09:50:30 +0900

    메인에 날씨 불러오기 완료

--- src/realmain.js
+++ src/realmain.js
@@ -17,21 +17,176 @@ import AutoHeightImage from 'react-native-auto-height-image';
 
 import LinearGradient from 'react-native-linear-gradient';
 
+import Geolocation from 'react-native-geolocation-service';
+import axios from 'axios';
+
+
 const chwidth = Dimensions.get('window').width
+
+
+
+const logo = require('../img/light/logo.png');
+const tuto = require('../img/light/tuto.png');
+const text1 = require('../img/light/menu1_2.png')
+const dark = require('../img/light/menu2_1.png')
+const menu = require('../img/light/menu3.png')
+const plus = require('../img/light/plus.png')
+
+const info1 = require('../img/light/info1.png');
+const info2 = require('../img/light/info2.png');
+const info3 = require('../img/light/info3.png');
 
 const Realmain = () => {
 
-    const logo = require('../img/light/logo.png');
-    const tuto = require('../img/light/tuto.png');
-    const text1 = require('../img/light/menu1_2.png')
-    const dark = require('../img/light/menu2_1.png')
-    const menu = require('../img/light/menu3.png')
-    const plus = require('../img/light/plus.png')
+    const [curTemp, setcurTemp] = useState(0)
+    const [curHumi, setcurHumi] = useState(0)
 
-    const info1 = require('../img/light/info1.png');
-    const info2 = require('../img/light/info2.png');
-    const info3 = require('../img/light/info3.png');
+    const [dayMaxTemp, setdayMaxTemp] = useState(0)
+    const [dayMinTemp, setdayMinTemp] = useState(0)
 
+    const [dayUv, setdayUv] = useState(0)
+    const [uvString, setUvString] = useState('')
+
+    const [pm10, setPm10] = useState(0)
+    const [pm25, setPm25] = useState(0)
+
+    const [totalAir, setTotalAir] = useState('측정중')
+
+    const [description, setdescription] = useState('')
+
+    useEffect(() => {
+
+        Geolocation.getCurrentPosition(
+            (position) => {
+
+                //오늘 최고 최저 온도 받아오기 및 자외선 지수
+                axios.get('https://api.openweathermap.org/data/2.5/onecall?lat=' + Math.round(position.coords.latitude * 100) / 100 + '&lon=' + Math.round(position.coords.longitude * 100) / 100 + '&exclude=current&appid=4c0e7c89ac35917a4adadc0c95b8392c',
+                ).then(function (response) {
+                    // console.log(response.data.daily[0])
+                    // console.log(response.data.daily[0].weather[0].icon)
+                    setdayUv(response.data.daily[0].uvi)
+                    setcurHumi(response.data.daily[0].humidity)
+                    setdayMaxTemp(response.data.daily[0].temp.max - 273.15)
+                    setdayMinTemp(response.data.daily[0].temp.min - 273.15)
+                    // setdescription(response.data.daily[0].weather[0].icon)
+
+                    if (response.data.daily[0].uvi < 2) {
+                        setUvString('낮음')
+                    } else if (response.data.daily[0].uvi >= 2 && response.data.daily[0].uvi < 5) {
+                        setUvString('보통')
+                    } else if (response.data.daily[0].uvi >= 5 && response.data.daily[0].uvi <= 7) {
+                        setUvString('높음')
+                    } else if (response.data.daily[0].uvi > 7) {
+                        setUvString('매우 높음')
+                    } else {
+                        setUvString('서버 오류')
+                    }
+
+                    switch (response.data.daily[0].weather[0].icon) {
+                        case '01d':
+                        case '01n':
+                            setdescription('맑음')
+                            break;
+
+                        case '02d':
+                        case '02n':
+                            setdescription('구름 조금')
+                            break;
+
+                        case '03d':
+                        case '03n':
+                            setdescription('구름 많음')
+                            break;
+
+                        case '04d':
+                        case '04n':
+                            setdescription('구름 많음')
+                            break;
+
+                        case '09d':
+                        case '09n':
+                            setdescription('소나기')
+                            break;
+
+                        case '10d':
+                        case '10n':
+                            setdescription('비')
+                            break;
+
+                        case '11d':
+                        case '11n':
+                            setdescription('뇌우')
+                            break;
+
+                        case '13d':
+                        case '13n':
+                            setdescription('눈')
+                            break;
+
+                        case '50d':
+                        case '50n':
+                            setdescription('안개')
+                            break;
+
+                    }
+
+                }).catch(function (error) {
+                    // handle error
+                    console.log(error);
+                }).then(function () {
+                    // always executed
+                });
+
+                //미세먼지 및 종합대기상황
+                axios.get('http://api.openweathermap.org/data/2.5/air_pollution?lat=' + Math.round(position.coords.latitude * 100) / 100 + '&lon=' + Math.round(position.coords.longitude * 100) / 100 + '&appid=4c0e7c89ac35917a4adadc0c95b8392c',
+                ).then(function (response) {
+                    console.log(response.data.list[0].components.pm10)
+                    console.log(response.data.list[0].components.pm2_5)
+                    console.log(response.data.list[0].main.aqi)
+
+                    setPm10(response.data.list[0].components.pm10)
+                    setPm25(response.data.list[0].components.pm2_5)
+
+
+                    switch (response.data.list[0].main.aqi) {
+                        case 1:
+                            setTotalAir('매우 좋음')
+                            break;
+                        case 2:
+                            setTotalAir('좋음')
+                            break;
+                        case 3:
+                            setTotalAir('보통')
+                            break;
+                        case 4:
+                            setTotalAir('나쁨')
+                            break;
+                        case 5:
+                            setTotalAir('매우 나쁨')
+                            break;
+
+                        default:
+                            setTotalAir('서버 오류')
+                            break;
+                    }
+
+                }).catch(function (error) {
+                    // handle error
+                    console.log(error);
+                }).then(function () {
+                    // always executed
+                });
+
+
+            },
+            (error) => {
+                // See error code charts below.
+                console.log(error.code, error.message);
+            },
+            { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+        );
+
+    }, [])
 
     return (
         <SafeAreaView style={{ width: '100%', height: '100%' }}>
@@ -60,7 +215,7 @@ const Realmain = () => {
                         <View style={{ height: 100, justifyContent: 'space-around', alignItems: 'center', marginTop: 5 }}>
                             <Text>자외선</Text>
                             <AutoHeightImage source={info1} width={chwidth / 9}></AutoHeightImage>
-                            <Text style={{ fontSize: 12, color: '#666666' }}>6 높음</Text>
+                            <Text style={{ fontSize: 12, color: '#666666' }}>{Math.round(dayUv)} {uvString}</Text>
                         </View>
                     </LinearGradient>
                 </View>
@@ -69,7 +224,7 @@ const Realmain = () => {
                     <View style={{ height: 100, justifyContent: 'space-around', alignItems: 'center', marginTop: 5 }}>
                         <Text>습도</Text>
                         <AutoHeightImage source={info2} width={chwidth / 11}></AutoHeightImage>
-                        <Text style={{ fontSize: 12, color: '#666666' }}>60%</Text>
+                        <Text style={{ fontSize: 12, color: '#666666' }}>{curHumi}%</Text>
                     </View>
                 </View>
 
@@ -78,7 +233,7 @@ const Realmain = () => {
                         <View style={{ height: 100, justifyContent: 'space-around', alignItems: 'center', marginTop: 5 }}>
                             <Text>미세먼지</Text>
                             <AutoHeightImage source={info3} width={chwidth / 10}></AutoHeightImage>
-                            <Text style={{ fontSize: 12, color: '#666666', letterSpacing: -0.8 }}>19㎍/m³ 좋음</Text>
+                            <Text style={{ fontSize: 12, color: '#666666', letterSpacing: -0.8 }}>{Math.round(pm10)}㎍/m³ {totalAir}</Text>
                         </View>
                     </LinearGradient>
                 </View>
@@ -87,11 +242,11 @@ const Realmain = () => {
                     <View style={{ height: 100, justifyContent: 'space-around', alignItems: 'center', marginTop: 5 }}>
                         <Text>오늘의 날씨</Text>
                         <View style={{ flexDirection: 'row' }}>
-                            <Text style={{ color: 'skyblue', fontSize: 13 }}>18°</Text>
+                            <Text style={{ color: 'skyblue', fontSize: 13 }}>{Math.round(dayMinTemp)}°</Text>
                             <Text>/</Text>
-                            <Text style={{ color: 'red', fontSize: 13 }}>25°</Text>
+                            <Text style={{ color: 'red', fontSize: 13 }}>{Math.round(dayMaxTemp)}°</Text>
                         </View>
-                        <Text style={{ fontSize: 12, color: '#666666' }}>구름 많음</Text>
+                        <Text style={{ fontSize: 12, color: '#666666' }}>{description}</Text>
 
                     </View>
                 </View>
--- src/weather.js
+++ src/weather.js
@@ -36,30 +36,16 @@ const Weather = () => {
         Geolocation.getCurrentPosition(
             (position) => {
 
-                // //현재 온도 받아오기
-                // axios.get('https://api.openweathermap.org/data/2.5/onecall?lat=' + Math.round(position.coords.latitude * 100) / 100 + '&lon=' + Math.round(position.coords.longitude * 100) / 100 + '&exclude=daily&appid=4c0e7c89ac35917a4adadc0c95b8392c',
-                // ).then(function (response) {
-                //     // setcurTemp((old) => Math.round(...old * 1000) / 1000)
-                // })
-                //     .catch(function (error) {
-                //         // handle error
-                //         console.log(error);
-                //     })
-                //     .then(function () {
-                //         // always executed
-                //     });
-
-
                 //오늘 최고 최저 온도 받아오기 및 자외선 지수
-                axios.get('https://api.openweathermap.org/data/2.5/onecall?lat=' + Math.round(position.coords.latitude * 100) / 100 + '&lon=' + Math.round(position.coords.longitude * 100) / 100 + '&exclude=current&lang=kr&appid=4c0e7c89ac35917a4adadc0c95b8392c',
+                axios.get('https://api.openweathermap.org/data/2.5/onecall?lat=' + Math.round(position.coords.latitude * 100) / 100 + '&lon=' + Math.round(position.coords.longitude * 100) / 100 + '&exclude=current&appid=4c0e7c89ac35917a4adadc0c95b8392c',
                 ).then(function (response) {
                     console.log(response.data.daily[0])
-                    console.log(response.data.daily[0].weather.description)
+                    console.log(response.data.daily[0].weather[0].icon)
                     setdayUv(response.data.daily[0].uvi)
                     setcurHumi(response.data.daily[0].humidity)
                     setdayMaxTemp(response.data.daily[0].temp.max - 273.15)
                     setdayMinTemp(response.data.daily[0].temp.min - 273.15)
-                    setdescription(response.data.daily[0].weather.description)
+                    // setdescription(response.data.daily[0].weather[0].icon)
 
                     if (response.data.daily[0].uvi < 2) {
                         setUvString('낮음')
@@ -71,6 +57,54 @@ const Weather = () => {
                         setUvString('매우 높음')
                     } else {
                         setUvString('서버 오류')
+                    }
+
+                    switch (response.data.daily[0].weather[0].icon) {
+                        case '01d':
+                        case '01n':
+                            setdescription('맑음')
+                            break;
+
+                        case '02d':
+                        case '02n':
+                            setdescription('구름 조금')
+                            break;
+
+                        case '03d':
+                        case '03n':
+                            setdescription('구름 많음')
+                            break;
+
+                        case '04d':
+                        case '04n':
+                            setdescription('구름 많음')
+                            break;
+
+                        case '09d':
+                        case '09n':
+                            setdescription('소나기')
+                            break;
+
+                        case '10d':
+                        case '10n':
+                            setdescription('비')
+                            break;
+
+                        case '11d':
+                        case '11n':
+                            setdescription('뇌우')
+                            break;
+
+                        case '13d':
+                        case '13n':
+                            setdescription('눈')
+                            break;
+
+                        case '50d':
+                        case '50n':
+                            setdescription('안개')
+                            break;
+
                     }
 
                 }).catch(function (error) {
